83e1c40a9b8e2b1fa8d8f9357af91c6379eef96f
Francesco <fra.iozzia@gmail.com>
2020-04-07 14:54:08 +0200

    struttura iniziale

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+// var contenitore = $("#central")
+// var bottone= $("#freccia");
+//
+// bottone.click(
+//
+//   function(){
+//     var input= ("input").value()
+//     contenitore.append("input")
+//   }
+//
+// )
+
+
+
+
+
+
+
+
+
+
+
+
+});
