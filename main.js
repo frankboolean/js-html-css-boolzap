e1e61437d396d4d6271436e230aec81c9b4308e3
Francesco <fra.iozzia@gmail.com>
2020-04-07 16:35:23 +0200

    con js

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
 $(document).ready(function(){
 
-// var contenitore = $("#central")
-// var bottone= $("#freccia");
-//
-// bottone.click(
-//
-//   function(){
-//     var input= ("input").value()
-//     contenitore.append("input")
-//   }
-//
-// )
+    var contenitore = $("#centrale");
+    var bottone= $("#freccia");
+
+
+    bottone.click(
+
+      function(){
+        var input= $("#scrivi").val();
+        contenitore.append("<li class=dentro>" + input + "</li>");
+         
+      }
+
+    )
 
 
 
 
+    $(".msg1").mouseenter( function(){
+      $(this).css("background-color","yellow");
+    }
+  );
 
-
-
-
-
-
+  $(".msg1").mouseleave( function(){
+    $(this).css("background-color","white");
+  }
+);
 
 
 });
